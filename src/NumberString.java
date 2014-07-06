ecde321b2dbc85d34871854031242d0ae4649587
rcarloz <mozety12@gmail.com>
2014-07-05 21:35:02 -0700

    initial commit

--- src/NumberString.java
+++ src/NumberString.java
@@ -1,6 +1,10 @@
 import java.lang.Object;
 
-
+/**
+ * 
+ * @author r-carloz
+ *
+ */
 public class NumberString extends Object {
 	public static void main(String[] args) {
 		String str = convertNumber("2054");
